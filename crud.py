7785004fc2a19a7685b9ee2bc6459540cd954f4a
oleoz1n <leopity0@gmail.com>
2023-11-21 07:53:08 -0300

    Alterando gitIgnore e mudando atributos das requisições sql no crud.py

--- crud.py
+++ crud.py
@@ -3,7 +3,7 @@ import json
 
 # Pegando o usuário e senha do arquivo
 
-path = r"C:\Users\leopi\Downloads\CRUD\CRUD\Aula3\login.json"
+path = r"./CRUD/CRUD/Aula3/login.json"
 with open(path, "r") as arquivo:
     dados = json.load(arquivo)
 
@@ -16,7 +16,7 @@ pswd = dados["password"]
 def connect():
     '''Caso a conexão já esteja feita, retorna a conexão existente. Caso contrário, cria uma nova conexão. Sem isso o programa não funciona'''
     try:
-        cx_Oracle.init_oracle_client(lib_dir=r"C:\Users\leopi\Downloads\CRUD\CRUD\instantclient_21_11")
+        cx_Oracle.init_oracle_client(lib_dir=r"./CRUD/CRUD/instantclient_21_11")
     except:
         pass
     dsn = cx_Oracle.makedsn(host="oracle.fiap.com.br", port=1521, sid="orcl")
@@ -26,7 +26,7 @@ def selectUsuario():
     conn  = connect()
     cursor = conn.cursor()
     
-    cursor.execute("SELECT id, nome, email, senha , TO_CHAR(nascimento, 'YYYY-MM-DD'), peso, altura FROM T_AC_USUARIO")
+    cursor.execute("SELECT id_usuario, nome, email, senha , TO_CHAR(dt_nasc, 'YYYY-MM-DD'), peso, altura FROM T_AC_USUARIO")
     linhas = cursor.fetchall()
     
     conn.commit()
@@ -39,7 +39,7 @@ def selectUsarioByEmail(email):
     conn  = connect()
     cursor = conn.cursor()
     
-    cursor.execute("SELECT id, nome, email, senha , TO_CHAR(nascimento, 'YYYY-MM-DD'), peso, altura FROM T_AC_USUARIO WHERE EMAIL=:valor1", valor1=email)
+    cursor.execute("SELECT id_usuario, nome, email, senha , TO_CHAR(dt_nasc, 'YYYY-MM-DD'), peso, altura FROM T_AC_USUARIO WHERE EMAIL=:valor1", valor1=email)
     linha = cursor.fetchall()
     
     conn.commit()
@@ -52,7 +52,7 @@ def existeUsuario(email):
     conn  = connect()
     cursor = conn.cursor()
     
-    cursor.execute("SELECT id, nome, email, senha , TO_CHAR(nascimento, 'YYYY-MM-DD'), peso, altura FROM T_AC_USUARIO WHERE EMAIL=:valor1", valor1=email)
+    cursor.execute("SELECT id_usuario, nome, email, senha , TO_CHAR(dt_nasc, 'YYYY-MM-DD'), peso, altura FROM T_AC_USUARIO WHERE EMAIL=:valor1", valor1=email)
     linha = cursor.fetchall()
     
     if len(linha) == 0:
@@ -69,7 +69,7 @@ def createUsuario(usuario):
     cursor = conn.cursor()
     
     try:
-        cursor.execute("INSERT INTO T_AC_USUARIO (NOME, EMAIL, SENHA, NASCIMENTO, PESO, ALTURA) VALUES (:valor1, :valor2, :valor3, TO_DATE(:valor4, 'YYYY-MM-DD'), :valor5, :valor6)",  valor1=usuario["nome"], valor2=usuario["email"], valor3=usuario["senha"], valor4=usuario["nascimento"], valor5=usuario["peso"], valor6=usuario["altura"])
+        cursor.execute("INSERT INTO T_AC_USUARIO (NOME, EMAIL, SENHA, dt_nasc, PESO, ALTURA) VALUES (:valor1, :valor2, :valor3, TO_DATE(:valor4, 'YYYY-MM-DD'), :valor5, :valor6)",  valor1=usuario["nome"], valor2=usuario["email"], valor3=usuario["senha"], valor4=usuario["nascimento"], valor5=usuario["peso"], valor6=usuario["altura"])
         conn.commit()
         cursor.close()
         conn.close()
@@ -82,7 +82,7 @@ def updateUsuario(email,usuario):
     conn  = connect()
     cursor = conn.cursor()
     try:
-        cursor.execute("UPDATE T_AC_USUARIO SET NOME=:valor1, EMAIL=:valor2, SENHA=:valor3, NASCIMENTO=TO_DATE(:valor4, 'YYYY-MM-DD'), PESO=:valor5, ALTURA=:valor6 WHERE EMAIL=:valor7",  valor1=usuario["nome"], valor2=usuario["email"], valor3=usuario["senha"], valor4=usuario["nascimento"], valor5=usuario["peso"], valor6=usuario["altura"], valor7=email)
+        cursor.execute("UPDATE T_AC_USUARIO SET NOME=:valor1, EMAIL=:valor2, SENHA=:valor3, dt_nasc=TO_DATE(:valor4, 'YYYY-MM-DD'), PESO=:valor5, ALTURA=:valor6 WHERE EMAIL=:valor7",  valor1=usuario["nome"], valor2=usuario["email"], valor3=usuario["senha"], valor4=usuario["nascimento"], valor5=usuario["peso"], valor6=usuario["altura"], valor7=email)
         conn.commit()
         cursor.close()
         conn.close()
@@ -96,7 +96,7 @@ def deleteUsuario(email):
     cursor = conn.cursor()
     
     try:
-        cursor.execute(("DELETE FROM T_AC_ACOES WHERE T_AC_ACOES.T_AC_USUARIO_ID = (SELECT T_AC_USUARIO.id FROM T_AC_USUARIO WHERE T_AC_USUARIO.email = :valor1)"), valor1=email)
+        cursor.execute(("DELETE FROM T_AC_ACOES WHERE T_AC_ACOES.T_AC_USUARIO_ID_USUARIO = (SELECT T_AC_USUARIO.id_usuario FROM T_AC_USUARIO WHERE T_AC_USUARIO.email = :valor1)"), valor1=email)
         cursor.execute("DELETE FROM T_AC_USUARIO WHERE EMAIL=:valor1", valor1=email)
         conn.commit()
         cursor.close()
@@ -110,7 +110,7 @@ def selectDicas():
     conn  = connect()
     cursor = conn.cursor()
     
-    cursor.execute("SELECT id, categoria, texto FROM T_AC_DICAS")
+    cursor.execute("SELECT id_dicas, categoria, texto FROM T_AC_DICAS")
     linhas = cursor.fetchall()
     
     conn.commit()
@@ -123,7 +123,7 @@ def selectDicasPorId(id):
     conn = connect()
     cursor = conn.cursor()
     
-    cursor.execute("SELECT id, categoria, texto FROM T_AC_DICAS WHERE id=:valor1", valor1=id)
+    cursor.execute("SELECT id_dicas, categoria, texto FROM T_AC_DICAS WHERE id_dicas=:valor1", valor1=id)
     linha = cursor.fetchall()
     
     conn.commit()
@@ -136,7 +136,7 @@ def dicaExiste(id):
     conn  = connect()
     cursor = conn.cursor()
     
-    cursor.execute("SELECT id, categoria, texto FROM T_AC_DICAS WHERE id=:valor1", valor1=id)
+    cursor.execute("SELECT id_dicas, categoria, texto FROM T_AC_DICAS WHERE id_dicas=:valor1", valor1=id)
     linha = cursor.fetchall()
     
     if len(linha) == 0:
@@ -168,7 +168,7 @@ def updateDica(id,dica):
     cursor = conn.cursor()
     
     try:
-        cursor.execute("UPDATE T_AC_DICAS SET categoria=:valor1, texto=:valor2 WHERE id=:valor3", valor1=dica["categoria"], valor2=dica["texto"], valor3=id)
+        cursor.execute("UPDATE T_AC_DICAS SET categoria=:valor1, texto=:valor2 WHERE id_dicas=:valor3", valor1=dica["categoria"], valor2=dica["texto"], valor3=id)
         conn.commit()
         cursor.close()
         conn.close()
@@ -183,7 +183,7 @@ def deleteDica(id):
     cursor = conn.cursor()
     
     try:
-        cursor.execute("DELETE FROM T_AC_DICAS WHERE id=:valor1", valor1=id)
+        cursor.execute("DELETE FROM T_AC_DICAS WHERE id_dicas=:valor1", valor1=id)
         conn.commit()
         cursor.close()
         conn.close()
